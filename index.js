124f1cbbae9422a098e39c958642a61cea2873e3
Daniel Moreno <danielmorenoa10@gmail.com>
2024-03-09 01:46:11 -0500

    subir archivos a GitHub

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import usuarioRoutes from "./routes/usuariosRoutes.js";
+//crear la app
+
+const app = express();
+
+//habilitar pug
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+//carpeta publica 
+
+app.use(express.static ('public'));
+
+//routing
+app.use("/auth", usuarioRoutes);
+
+//definir puerto y arrancar proyecto
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`El servidor esta corriendo el puerto ${port}`);
+});
